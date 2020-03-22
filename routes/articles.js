cfea76d6ce16a63f6fd132697b0ec87cf2d9f975
Alif Munim <alif.munim@ryerson.ca>
2020-03-21 21:07:29 -0400

    Add access control for edit and delete functions

--- routes/articles.js
+++ routes/articles.js
@@ -2,19 +2,25 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
-// Bring in Models
+// Article Model
 let Article = require("../models/article")
+// User Model
+let User = require("../models/user");
 
 // Add Route
-router.get("/add", function(req, res){
+router.get("/add", ensureAuthenticated, function(req, res){
   res.render("add_article", {
     title: "Add Articles"
   });
 });
 
 // Load Edit Form
-router.get("/edit/:id", function(req, res){
+router.get("/edit/:id", ensureAuthenticated, function(req, res){
   Article.findById(req.params.id, function(err, article){
+    if(article.author != req.user._id) {
+      req.flash("danger", "Not authorized");
+      res.redirect("/");
+    }
     res.render("edit_article", {
       title: "Edit Article",
       article: article
@@ -27,16 +33,10 @@ router.get("/edit/:id", function(req, res){
 router.post('/add',
  [
   check('title').isLength({min:1}).trim().withMessage('Title required'),
-  check('author').isLength({min:1}).trim().withMessage('Author required'),
+  // check('author').isLength({min:1}).trim().withMessage('Author required'),
   check('body').isLength({min:1}).trim().withMessage('Body required')
  ],
  (req,res,next)=>{
-
-    let article = new Article({
-      title:req.body.title,
-      author:req.body.author,
-      body:req.body.body
-    });
 
     const errors = validationResult(req);
 
@@ -48,8 +48,9 @@ router.post('/add',
         errors: errors.mapped()
       });
     } else {
+      let article = new Article();
       article.title = req.body.title;
-      article.author = req.body.author;
+      article.author = req.user._id;
       article.body = req.body.body;
 
       article.save(err=>{
@@ -84,21 +85,49 @@ router.post("/edit/:id", function(req, res){
 // Get Single Article
 router.get("/:id", function(req, res){
   Article.findById(req.params.id, function(err, article){
-    res.render("article", {
-      article: article
-    });
+    User.findById(article.author, function(err, user){
+      res.render("article", {
+        article: article,
+        author: user.name
+      });
+    })
+
   });
 });
 
 // Delete an Article
 router.delete("/:id", function(req, res){
+
+  if(!req.user._id){
+    res.status(500).send();
+  }
+
   let query = {_id: req.params.id}
-  Article.remove(query, function(err){
-    if(err){
-      console.log(err);
+
+  Article.findById(req.params.id, function(err, article){
+    if(article.author != req.user._id){
+      res.status(500).send();
+    } else {
+      Article.remove(query, function(err){
+        if(err){
+          console.log(err);
+        }
+        res.send("Success");
+      });
     }
-    res.send("Success");
-  })
+  });
+
+
 });
+
+// Access control
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    req.flash("danger", "Please login");
+    res.redirect("/users/login");
+  }
+}
 
 module.exports = router;
