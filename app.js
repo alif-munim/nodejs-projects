281b70ca0bdf76412413d717a76952e9092c8bb5
Alif Munim <alif.munim@ryerson.ca>
2020-03-22 11:10:59 -0400

    Update title

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get('/', function(req, res){
       console.log(err);
     } else {
       res.render("index", {
-        title: "KnowledgeBase",
+        title: "Bibly",
         articles: articles
       });
     }
